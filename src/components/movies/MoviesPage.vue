69ae5700d459d3076f083d8f5a2172b02dda6ed0
Manolo Rodriguez <manolo.rodriguez@wizeline.com>
2018-07-22 22:20:53 -0500

    Add basic vuex setup

--- src/components/movies/MoviesPage.vue
+++ src/components/movies/MoviesPage.vue
@@ -1,3 +1,15 @@
 <template>
-  <h1>Hello world from movies</h1>
+  <div class="container">
+    <h1>{{ message }}</h1>
+  </div>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  computed: mapState({
+    message: state => state.message
+  })
+}
+</script>
